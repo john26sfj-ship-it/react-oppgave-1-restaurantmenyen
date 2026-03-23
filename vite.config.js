ca0ba96b42c6803bff8dd0ad9b86ed608df65b56
John <john.26sfj@gmail.com>
2026-03-23 10:02:39 +0100

    test vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/react-oppgave-1-restaurantmenyen/`,
+  // base: `/react-oppgave-1-restaurantmenyen/`,
 });
