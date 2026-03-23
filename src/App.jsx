2bfb11727a2f94177e87f94b98e2882734915a29
John <john.26sfj@gmail.com>
2026-03-23 09:37:31 +0100

    moved array import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <Menu />
+      <Menu data={menuArr} />
     </>
   )
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,17 +2,17 @@
 import Dish from "./Dish.jsx";
 // Import array of dishes.
 // Different variable name to show it´s not a component?
-import menuArr from "./MenuArray.jsx";
+// import menuArr from "./MenuArray.jsx";
 import styles from "./css-files/Menu.module.css";
 // Export for use in App.jsx
-export default function Menu() {
+export default function Menu({data}) {
     return (
         // Curlies to use js in "html".
         // Map menu to arrow function.
         // Each menuitem sent to Dish as props, using spread.
         // Why parentheses instead of curlies for arrow block?
         <div className={styles.container}>
-            {menuArr.map((menuItem) => (
+            {data.map((menuItem) => (
                 <Dish key={menuItem.id} {...menuItem} />
             ))}
         </div>
